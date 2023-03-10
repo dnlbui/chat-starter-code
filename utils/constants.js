0946462070c422a671e695feee8d1b5d9a43be19
Daniel Bui <dnlbui@proton.me>
2023-03-10 09:28:42 -0600

    comments about module

--- utils/constants.js
+++ utils/constants.js
@@ -7,6 +7,11 @@ const CLIENT = {
 };
 
 // This check allows the module to be used in the client and the server
+// module is undefined in the client. module.exports is undefined in the server.
+// This is a common pattern to check if the module is being used in the client or the server.
+// module contains all the code that is being exported from the module.
+// module.exports is the object that is being exported from the module.
+// exports is a reference to module.exports.
 if (typeof module !== "undefined" && module.exports) {
   module.exports = exports = {
     PORT,
